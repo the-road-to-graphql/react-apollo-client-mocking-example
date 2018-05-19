7b01805bb1c6cefc36d1b17d803120b9ead3ed6a
Robin Wieruch <wrobin@gmx.net>
2018-05-19 20:55:26 +0800

    add mutation mock

--- src/schema.js
+++ src/schema.js
@@ -6,14 +6,75 @@ export const typeDefs = `
   type Organization {
     name: String!
     url: String!
+    repositories: RepositoryConnection!
+  }
+
+  type RepositoryConnection {
+    edges: [RepositoryEdge!]!
+  }
+
+  type RepositoryEdge {
+    node: Repository!
+  }
+
+  type Repository {
+    id: String!
+    name: String!
+    url: String!
+    viewerHasStarred: Boolean!
+  }
+
+  type Mutation {
+    addStar(input: AddStarInput!): AddStarResult!
+  }
+
+  input AddStarInput {
+    starrableId: ID!
+  }
+
+  type AddStarResult {
+    starrable: Starrable!
+  }
+
+  type Starrable {
+    id: ID!
+    viewerHasStarred: Boolean!
   }
 `;
 
 export const resolvers = {
+  Mutation: {
+    addStar: (parent, { input }) => ({
+      starrable: {
+        id: input.starrableId,
+        viewerHasStarred: true,
+      },
+    }),
+  },
   Query: {
     organization: (parent, { login }) => ({
       name: login,
       url: `https://github.com/${login}`,
+      repositories: {
+        edges: [
+          {
+            node: {
+              id: '1',
+              name: 'the-road-to-learn-react',
+              url: `https://github.com/${login}/the-road-to-learn-react`,
+              viewerHasStarred: false,
+            },
+          },
+          {
+            node: {
+              id: '2',
+              name: 'the-road-to-learn-react-chinese',
+              url: `https://github.com/${login}/the-road-to-learn-react-chinese`,
+              viewerHasStarred: false,
+            },
+          },
+        ],
+      },
     }),
   },
 };
